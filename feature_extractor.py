17831cafe5df38bfba0eda766a83c019b56627e1
woodminus <160528767+woodminus@users.noreply.github.com>
2020-02-08 18:47:19 +0000

    Gripe.

--- feature_extractor.py
+++ feature_extractor.py
@@ -16,3 +16,33 @@ from keras.layers import Activation
 from keras.layers import Input, Lambda, Dense, Dropout, Convolution2D, MaxPooling2D, Flatten,Concatenate,Reshape
 from keras.models import Sequential, Model
 from keras.optimizers import RMSprop
+
+import tensorflow as tf
+
+def image_feat_network(input_shape):
+   seq = Sequential()
+   seq.add(Convolution2D(32, kernel_size=(3,3), input_shape=(56,56,1)))
+   seq.add(Activation('relu'))
+   seq.add(MaxPooling2D(pool_size=(2, 2)))
+   seq.add(Dropout(.25))
+   return seq
+
+def audio_feat_network(input_shape):
+    seq = Sequential()
+    seq.add(Convolution2D(32, kernel_size=(3,3), input_shape=(1025,47,1)))
+    seq.add(Activation('relu'))
+    seq.add(MaxPooling2D(pool_size=(4, 2)))
+    seq.add(Dropout(.25))
+    seq.add(Convolution2D(32, kernel_size=(3,3)))
+    seq.add(Activation('relu'))
+    seq.add(MaxPooling2D(pool_size=(4, 2)))
+    seq.add(Convolution2D(32, kernel_size=(4,1)))
+    seq.add(Activation('relu'))
+    seq.add(MaxPooling2D(pool_size=(1, 1)))
+    seq.add(Convolution2D(32, kernel_size=(4,1)))
+    seq.add(Activation('relu'))
+    seq.add(MaxPooling2D(pool_size=(1, 1)))
+    seq.add(Convolution2D(32, kernel_size=(3,1)))
+    seq.add(Activation('relu'))
+    seq.add(MaxPooling2D(pool_size=(2, 1)))
+    return seq
