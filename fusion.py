67070c8594e5cf0547baa2181d413dbeabaf5a95
woodminus <160528767+woodminus@users.noreply.github.com>
2020-03-01 11:02:39 +0000

    Update #3

--- fusion.py
+++ fusion.py
@@ -12,4 +12,5 @@ from sklearn.model_selection import train_test_split
 from keras import backend as K
 from keras.layers import Activation
 from keras.layers import Input, Lambda, Dense, Dropout, Convolution2D, MaxPooling2D, Flatten,Concatenate,Reshape
-from keras.models impor
+from keras.models import Sequential, Model
+f
