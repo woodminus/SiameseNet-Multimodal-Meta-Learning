3acb6dd1414de99a7f99afaf38499011b0c1b65f
woodminus <160528767+woodminus@users.noreply.github.com>
2020-03-07 11:57:18 +0000

    Added Screenshot

--- fusion.py
+++ fusion.py
@@ -20,4 +20,5 @@ import tensorflow as tf
 def multi_modal_network(input_shape):
    kernel_size = 3
    seq = Sequential()
-   seq.add(Convolution2D(6, kernel_size=(3,3), input_sha
+   seq.add(Convolution2D(6, kernel_size=(3,3), input_shape=(27, 37, 32)))
+   seq.add(A
