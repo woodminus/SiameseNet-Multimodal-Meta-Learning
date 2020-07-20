3289cd5bf398092fe54037561ffc4823af69e241
woodminus <160528767+woodminus@users.noreply.github.com>
2020-07-20 11:15:30 +0000

    added a readme file

--- model.py
+++ model.py
@@ -41,4 +41,4 @@ def siamese_network(input_dim_img,input_dim_aud):
     input_dim = (27,37,32)
     base_network = fusion.multi_modal_network(input_dim)
     feat_vecs_a = base_network(concat_a)
-    feat_vecs_b = b
+    feat_vecs_b = base_network(concat_b)
