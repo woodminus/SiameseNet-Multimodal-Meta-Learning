6f8cb5d0ad8929c0c3e1a4916a1bd3b152f37fea
woodminus <160528767+woodminus@users.noreply.github.com>
2020-09-24 17:13:20 +0000

    6.1.4

--- model.py
+++ model.py
@@ -54,4 +54,5 @@ def euclidean_distance(vects):
     return K.sqrt(K.sum(K.square(x - y), axis=1, keepdims=True))
 
 
-def eucl_dist_output_shape(sha
+def eucl_dist_output_shape(shapes):
+    shape1, shape2 =
