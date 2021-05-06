674a627569321b07d733e9d9ddfde82fe975f4b2
woodminus <160528767+woodminus@users.noreply.github.com>
2021-05-06 06:55:30 +0000

    0.0.65

--- model.py
+++ model.py
@@ -62,4 +62,5 @@ def contrastive_loss(y_true, y_pred):
     margin = 1
     return K.mean(y_true * K.square(y_pred) + (1 - y_true) * K.square(K.maximum(margin - y_pred, 0)))
 
-def compute_accuracy(predictions
+def compute_accuracy(predictions, labels):
+    return 
